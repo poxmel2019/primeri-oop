32378d4aec8f6240518f3007d759fe0e95cb49b1
Madiyar <nu_ofsn11@mail.ru>
2020-02-27 09:57:46 +0600

    Score get, user's answer checking and handling

--- primeri-oop/Program.cs
+++ primeri-oop/Program.cs
@@ -11,6 +11,7 @@ namespace primeri_oop
     class User : Examples
     {
         private int _scores = 0;
+        private int[] _userAnswers = new int[4];
         public int Scores
         {
             get
@@ -18,40 +19,72 @@ namespace primeri_oop
                 Console.WriteLine("User scores: ");
                 return _scores;
             }
+            /*
             set
             {
                 _scores = value;
             }
+            */
+        }
 
+        public int[] UserAnswers
+        {
+            get;
+            set;
         }
        
 
         public int userRightAnswersAmount = 0;
 
-        int userAnswer = 0;
+        string userAnswer = null;
         int[] userAnswers = new int[5];
         public User()
         {
-            Scores = 0;
-            
-            
+            _scores = 0;
+          
         }
 
         public void SolveExercises()
         {
+            //double handledAnswer = 0;
             for (int i = 0; i < exercises.Length; i++)
             {
-                Console.WriteLine(exercises[i]);
-                userAnswer = Convert.ToInt32(Console.ReadLine());
-                userAnswers[i] = userAnswer;
-                if (userAnswers[i] == rightAnswers[i])
+                while (true)
                 {
-                    userRightAnswersAmount++;    
+                    Console.WriteLine(exercises[i]);
+                    try
+                    {
+                        userAnswer = Console.ReadLine();
+                        double handledAnswer = Convert.ToDouble(userAnswer);
+                        userAnswers[i] = (int)handledAnswer;
+                        if (userAnswers[i] == rightAnswers[i])
+                        {
+                            userRightAnswersAmount++;
+                        }
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        if (string.IsNullOrEmpty(userAnswer) || string.IsNullOrWhiteSpace(userAnswer))
+                        {
+                            Console.WriteLine("You entered nothing!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Number please");
+                        }
+                    }
+                    catch (OverflowException)
+                    {
+                        Console.WriteLine("Number is too big!");
+                    }
+                    
                 }
+                
             }
             if (userRightAnswersAmount == exercises.Length)
             {
-                Scores++;
+                _scores++;
             }
             
         }
@@ -65,6 +98,7 @@ namespace primeri_oop
             User me = new User();
             //Console.WriteLine(me.Scores);
             me.SolveExercises();
+            //me.Scores = 10;
             Console.WriteLine(me.Scores);
 
 
